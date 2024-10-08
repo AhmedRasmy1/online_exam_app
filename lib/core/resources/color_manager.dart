0f26b7f553f59ba053dfa265e561a0cc860cb6da
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-10-08 22:05:57 +0300

    creat color manager resources @AhmedRasmy1

---
+++ lib/core/resources/color_manager.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class ColorManager {
+  static const Color blue = Color(0xff02369c); // for primary color
+  static const Color lightBlue = Color(0xffEDEFF3);
+  static const Color grey = Color(0xff535353); // for subtitles color
+  static const Color darkgrey = Color(0xff7a7a7c);
+  static const Color red = Color(0xffCC1010); // for errors and warnings
+  static const Color lightRed = Color(0xffF8D2D2); //
+  static const Color black = Color(0xff0F0F0F); // for text color
+  static const Color placeHolderColor = Color(0xff535353);
+  static const Color green = Color(0xff11CE19); // for success
+  static const Color lightGreen = Color(0xffCAF9CC);
+  static const Color colorOfEmailverification = Color(0xffDFE7F7);
+}
