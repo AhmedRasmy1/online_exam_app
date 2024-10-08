9676a822e93813ac5f4e4ccb92ca4729960efcbb
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-10-08 22:34:51 +0300

    create strings manager

---
+++ lib/core/resources/strings_manager.dart
@@ -0,0 +1,32 @@
+class StringsManager {
+  static const String login = "Login";
+  static const String email = "Email";
+  static const String enterYourEmail = "Enter your email";
+  static const String password = "Password";
+  static const String enterYourPassword = "Enter your password";
+  static const String loginButton = "Login";
+  static const String signUp = "Sign Up";
+  static const String alreadyHaveAccount = "Already have an account?";
+  static const String dontHaveAccount = "Don't have an account?";
+  static const String rememberMe = "Remember Me";
+  static const String forgetPassword = "Forget Password?";
+  static const String userName = "User Name";
+  static const String enterYourUserName = "Enter your user name";
+  static const String firstName = "First Name";
+  static const String enterYourFirstName = "Enter first name";
+  static const String lastName = "Last Name";
+  static const String enterYourLastName = "Enter first name";
+  static const String phoneNumber = "phone Number";
+  static const String enterPhoneNumber = "Enter phone number";
+  static const String confirmPassword = "Confirm Password";
+  static const String enterYourConfirmPassword = "confirm password";
+  static const String titleOfForgetPassword = "Forget password";
+  static const String subTitleOfForgetPassword =
+      "Please enter your email associated to your account";
+  static const String dontreciveCode = "Don't receive code?";
+  static const String titleOfResetPassword = "Reset password";
+  static const String subTitleOfResetPassword =
+      "Password must not be empty and must contain 6 characters with upper case letter and one number at least ";
+  static const String resend = "Resend";
+  static const String newPassword = "New Password";
+}
