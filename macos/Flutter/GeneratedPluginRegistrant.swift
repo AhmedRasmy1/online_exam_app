8a0a9697b47401f862273f59efd3d4a5aaaecf9d
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-10-14 09:38:42 +0300

    fix merrgeeeeeeeeeeeeeeeee

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-
-import shared_preferences_foundation
+import rive_common
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-
-
+  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
 }
