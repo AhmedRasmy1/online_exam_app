b2b303d9b045ae3704b312f39198eba6730d03c4
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-10-18 08:06:38 +0300

    refactor login use case

--- lib/Features/Auth/domain/use_cases/login_usecase.dart
+++ lib/Features/Auth/domain/use_cases/login_usecase.dart
@@ -1,7 +1,5 @@
 import 'package:injectable/injectable.dart';
-
 import '../common/api_result.dart';
-import '../entities/forgot_password_entities.dart';
 import '../entities/user.dart';
 import '../repositories/auth_repo.dart';
 
@@ -13,21 +11,5 @@ class LoginUseCase {
 
   Future<Result<User?>> invoke(String email, String password) {
     return authRepo.login(email, password);
-  }
-
-  Future<Result<User?>> register(
-      String username,
-      String firstName,
-      String lastName,
-      String email,
-      String password,
-      String rePassword,
-      String phone) {
-    return authRepo.register(
-        username, firstName, lastName, email, password, rePassword, phone);
-  }
-
-  Future<Result<ForgotPasswordEntities?>> forgotPassword(String email) {
-    return authRepo.forgotPassword(email: email);
   }
 }
