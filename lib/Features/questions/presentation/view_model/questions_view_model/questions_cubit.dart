47c231c13ade18f8e19fd2b8cf77f21f62d873ff
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-11-01 20:41:52 +0200

    Enhance question handling by adding support for single and multiple choice answers in QuestionsCubit and updating QuestionsView for state management

--- lib/Features/questions/presentation/view_model/questions_view_model/questions_cubit.dart
+++ lib/Features/questions/presentation/view_model/questions_view_model/questions_cubit.dart
@@ -1,4 +1,3 @@
-import 'dart:developer';
 import 'package:bloc/bloc.dart';
 import 'package:injectable/injectable.dart';
 import 'package:meta/meta.dart';
@@ -11,30 +10,39 @@ part 'questions_state.dart';
 @injectable
 class QuestionsCubit extends Cubit<QuestionsState> {
   final QuestionUseCase _questionUseCase;
-  List<QuestionEntity> questionsList = []; //! to store the questions
-  int currentIndex = 0; //! to store the current index of the question
+  List<QuestionEntity> questionsList = [];
+  int currentIndex = 0;
+  List<int?> singleChoiceAnswers = []; // For single-choice answers
+
+  List<List<bool>> multiChoiceAnswers = []; // For multiple-choice answers
+
   QuestionsCubit(this._questionUseCase) : super(QuestionsInitial());
 
   Future<void> getQuestionById() async {
     emit(QuestionsLoading());
     var token = SharedData.getData(key: StringCache.userToken);
-    log("token: $token");
     var id = SharedData.getData(key: StringCache.questionId);
-    log("id: $id");
     final result = await _questionUseCase.getQuestionById(id, token);
-    switch (result) {
-      case Success<List<QuestionEntity>>():
-        {
-          questionsList = result.data;
-          log(questionsList[currentIndex].toString());
-          emit(QuestionsSuccess(questionsList)); //! for show the first question
-        }
-      case Fail<List<QuestionEntity>>():
-        {
-          log(result.exception.toString());
-          emit(QuestionsFail(result.exception));
-        }
+    if (result is Success<List<QuestionEntity>>) {
+      questionsList = result.data;
+      singleChoiceAnswers = List.filled(questionsList.length, null);
+      multiChoiceAnswers =
+          List.generate(questionsList.length, (_) => List.filled(4, false));
+      emit(QuestionsSuccess(questionsList));
+    } else if (result is Fail<List<QuestionEntity>>) {
+      emit(QuestionsFail(result.exception));
     }
+  }
+
+  void selectSingleChoiceAnswer(int questionIndex, int answerIndex) {
+    singleChoiceAnswers[questionIndex] = answerIndex;
+    emit(QuestionsSuccess(questionsList));
+  }
+
+  void selectMultiChoiceAnswer(
+      int questionIndex, int answerIndex, bool isSelected) {
+    multiChoiceAnswers[questionIndex][answerIndex] = isSelected;
+    emit(QuestionsSuccess(questionsList));
   }
 
   void nextQuestion() {
